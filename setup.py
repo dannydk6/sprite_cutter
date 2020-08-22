43e191ba1c1a50579ccb929989eb98f6097f35e7
Daniel Alarcon <dfa238@nyu.edu>
2020-08-22 15:57:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dannydk6/sprite_cutter",
     packages=setuptools.find_packages(),
+    license="GNU Affero General Public License v3.0",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
